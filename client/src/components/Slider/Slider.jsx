077f24a50912846324928b3c18aaa90af4781920
Juliya <juliyaone123@gmail.com>
2022-03-17 14:17:14 +0300

    create nav and slider

---
+++ client/src/components/Slider/Slider.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Slider(props) {
+
+  const images = ["https://images.unsplash.com/photo-1647003810228-ba8324796acf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80", "https://images.unsplash.com/photo-1647003613495-b5f9a858365b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80", "https://images.unsplash.com/photo-1615758043606-084300e6439b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80", "https://images.unsplash.com/photo-1638913976954-8f7b612867c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80", "https://images.unsplash.com/photo-1646991838618-7d7147658f37?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80", "https://images.unsplash.com/photo-1646988423425-bc236bc449bd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80", "https://images.unsplash.com/photo-1646928998297-5fb2a10aca27?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80" ];
+
+  return (
+    <div>
+    {/* //   <div className="container">
+    //   { 
+    //   images.forEach((slide, index) =>{ 
+    //     {images.map((el) => <img className="slide active" width="640" height="480" key="el.id" src={`${images[index]`}} alt="cat"/>)}
+
+        
+    //   })
+
+    //   </div> */}
+    </div>
+  );
+}
+
+export default Slider;
