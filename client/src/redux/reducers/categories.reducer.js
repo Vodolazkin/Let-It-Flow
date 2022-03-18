4e89c92d8a9be04507ee9f0afc7250b6d2b21738
Juliya <juliyaone123@gmail.com>
2022-03-18 18:05:42 +0300

    categories-img

--- client/src/redux/reducers/categories.reducer.js
+++ client/src/redux/reducers/categories.reducer.js
@@ -2,7 +2,7 @@ export const categoriesReducer = (state = [], action) => {
   switch (action.type) {
 
     case 'INIT_CATEGORIES':{
-      return [...state, ...action.payload];
+      return [...action.payload];
     }
    
     default:
