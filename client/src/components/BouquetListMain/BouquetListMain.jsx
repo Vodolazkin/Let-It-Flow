2c5b978bd5e3cd6e867c3029321dc76cd56dfee0
Darina <jiva108jiva@gmail.com>
2022-03-18 14:30:52 +0300

    nichego novogo

--- client/src/components/BouquetListMain/BouquetListMain.jsx
+++ client/src/components/BouquetListMain/BouquetListMain.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux'
 
 function Card(props) {
+  // const { bouquetsRe } = useSelector((state) => state)
   
+  const dispatch = useDispatch()
+useEffect(() =>{
+fetch('http://localhost:4000/bouquets')
+.then(res => res.json())
+// .then(data => dispatch({ type: 'INIT_BOUQUETS', payload: data}))
+.then(data => console.log(data))
+.catch(err=>console.log(err));
+},[])
   return (
     <div>
-      jyj,k
+     {/* {bouquetsRe.map(bouquet => (
+
+     ))} */}
     </div>
   );
 }
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,11 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore } from 'redux'
 import { rootReducer } from ''
 import { composeWithDevTools } from 'redux-devtools-extension'
-import thunk from 'redux-thunk'
+//import thunk from 'redux-thunk'
 
 const initialStore = {
   user: {},
+  bouquetsRe: []
 }
  
-export const store = createStore(rootReducer, initialStore, composeWithDevTools(applyMiddleware(thunk)))
+export const store = createStore(rootReducer, initialStore, composeWithDevTools())
