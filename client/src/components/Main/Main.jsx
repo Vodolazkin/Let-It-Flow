0850456cbe343be0179e64a04f20dbc4d00443ad
Juliya <juliyaone123@gmail.com>
2022-03-19 10:05:22 +0300

    MapYandex

--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Categories from '../Categories/Categories';
 import Slider from '../Slider/Slider';
+import YandexMap from '../YandexMap/YandexMap';
 
 function Main(props) {
   return (
     <>
       <Slider />
       <Categories />
+      <YandexMap />
     </>
   );
 }
