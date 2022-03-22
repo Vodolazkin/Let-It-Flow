6a51d16690d67a10fb45556e52bfe683ebf31035
Anzhelika <mrstonem@mail.ru>
2022-03-22 22:18:20 +0300

    add bouquet

--- server/routes/card.routes.js
+++ server/routes/card.routes.js
@@ -9,4 +9,20 @@ router.get('/:id', async (req, res) => {
 })
 
 
+router.post('/',  async (req, res) => {
+  try {
+    const { title, description, price, img, category_id } = req.body;
+ 
+    const bouquet = await Bouquet.create({ title, description, price, img, category_id });
+    return res.json(bouquet)
+    
+  } catch (error) {
+    res.status(401)
+      .json({
+        message: error.message,
+      }).end();
+  }
+});
+
+
 module.exports = router;
