971403749b732239cfa89623092f0443b547475e
Anzhelika <mrstonem@mail.ru>
2022-03-23 17:45:24 +0300

    add bouquet admin

--- client/src/redux/reducers/boquets.reducer.js
+++ client/src/redux/reducers/boquets.reducer.js
@@ -1,3 +1,11 @@
+import {
+  ADD_BOUQUET,
+  DELETE_BOUQUET,
+  UPDATE_BOUQUET,
+  INIT_CATEGORY_BOUQUETS,
+  INIT_BOUQUETS
+} from '../actionType/bouquetActionType'
+
 const initialState = {
   bouquetsRe: [],
 }
@@ -6,13 +14,38 @@ export function bouquetsReducer(state = initialState, action) {
   
 
   switch (action.type) {
-    case 'INIT_BOUQUETS': {
+
+    case INIT_BOUQUETS: {
       return {...state, bouquetsRe: action.payload}
     }
-    case 'INIT_CATEGORY_BOUQUETS': {
-      // return [...action.payload]
+
+    case INIT_CATEGORY_BOUQUETS: {
       return {...state, bouquetsRe: action.payload}
     }
+
+    case ADD_BOUQUET:
+      return [...state.bouquetsRe, action.payload]
+
+    case DELETE_BOUQUET:
+      return { ...state, cards: state.cards.filter(el => el.id !== action.payload)}
+
+    case UPDATE_BOUQUET:
+      return { ...state, cards: state.cards.map(el => {
+        if (el.id === action.payload.id) {
+          return {
+            ...el, 
+            id: action.payload.id,
+            title: action.payload.title, 
+            description: action.payload.description,
+            image: action.payload.image,
+            price: action.payload.price,
+            category_id: action.payload.category_id,
+          }
+        } else {
+          return el
+        }
+      })}
+
     default: {
       return state
     }
