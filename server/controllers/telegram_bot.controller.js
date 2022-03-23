f830c54be2110a0abb2bf05cd2f580342ee49bc8
Darina <jiva108jiva@gmail.com>
2022-03-23 17:30:09 +0300

    bot 0

--- server/controllers/telegram_bot.controller.js
+++ server/controllers/telegram_bot.controller.js
@@ -16,8 +16,11 @@ const order = await Order.findAll({
        [Op.between]: [(new Date()), nearestMidnight],
       //  delivery_method : 'delivery'
    }
+   
 }
+
 })
+console.log(order,'jhhh');
 order.map((order) => ctx.reply(`
 Дата доставки: ${(JSON.stringify(order.delivery_date)).slice(1 ,11)}
 Время доставки: ${order.delivery_time}
