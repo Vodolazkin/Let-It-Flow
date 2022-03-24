5ddb9ece0835a5e41c06dbe411aa795c31b01c84
Anzhelika <mrstonem@mail.ru>
2022-03-24 12:16:34 +0300

    admin add-update-delete

--- server/routes/bouquets.routes.js
+++ server/routes/bouquets.routes.js
@@ -47,20 +47,39 @@ router.post('/',  async (req, res) => {
 
 
 router.post('/edit/:id',  async (req, res) => {
+
+  if (req.files === null) {
+    return res.status(400).json({ msg: 'No file uploaded' });
+  }
+ 
+  const sampleFile = req.files.file;
+
+  
+  let uploadPath = '/img/' + sampleFile.name;
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+  sampleFile.mv('public/img/'+sampleFile.name, function(err) {
+    if (err)
+      console.log(err);
+  });
+
   try {
     const { id } = req.params;
-    const { title, description, price, img, category_id } = req.body;
+    const { title, description, price, category_id } = req.body;
+    console.log(title, description, price, category_id)
  
-    const bouquet = Bouquet.findOne({
+    const bouquet = await Bouquet.findOne({
       where: { id }
     })
 
     bouquet.title = title
     bouquet.description = description
-    bouquet.price
-    bouquet.img
-    bouquet.category_id
-    bouquet.save()
+    bouquet.price = price
+    bouquet.img = uploadPath
+    bouquet.category_id = category_id
+    console.log(bouquet)
+    await bouquet.save()
 
     return res.json(bouquet)
     
