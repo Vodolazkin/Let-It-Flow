0dcb7abf91017adbfd2cb5e5bb0bb645a3ce9954
Artem Tarasov <tarasowar@gmail.com>
2022-03-29 00:52:04 +0300

    dov

--- server/app.js
+++ server/app.js
@@ -30,7 +30,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload());
-app.use(express.static(path.resolve(__dirname, '../client/build/')))
+app.use(express.static(path.resolve(__dirname, 'build')))
 
 const corsConfig = {
   origin: true,
