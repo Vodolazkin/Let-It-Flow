84870de8ffeee026163eee3d36156f5db6dbbd41
Artem Tarasov <tarasowar@gmail.com>
2022-03-29 00:03:28 +0300

    reer

--- server/app.js
+++ server/app.js
@@ -28,7 +28,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload());
-app.use(express.static(path.join(process.env.PWD, 'build')))
+app.use(express.static(path.resolve(__dirname, '../client/build/index.html')))
 
 const corsConfig = {
   origin: true,
